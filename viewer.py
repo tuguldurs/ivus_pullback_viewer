7a351e0be792937eb2a4e8b44916b1f8b39fc0b5
tuguldurs <tuguldur.s@gmail.com>
2022-02-08 22:14:14 -0500

    pullback class

--- viewer.py
+++ viewer.py
@@ -2,15 +2,31 @@ from __future__ import annotations
 
 
 import argparse
-
+from dataclasses import dataclass
 
 from pydicom import dcmread
 
 
+@dataclass
+class PullBack:
+    """Reads input DICOM series instance.
+
+    Attributes:
+        series_name - Path to DICOM series instance.
+        dcm - pydicom.dataset.Filedataset object.
+        video - numpy array of individual frames.
+    """
+
+    series_name: str
+
+    def __post_init__(self) -> None:
+        self.dcm = dcmread(self.series_name)
+        self.video = self.dcm.pixel_array
+
+
 def main(args) -> None:
     """Drives the viewer."""
-    dcm = dcmread(args.series_name)
-    ivus = dcm.pixel_array
+    pb = PullBack(args.series_name)
 
 
 if __name__ == '__main__':
