24333032d18a5b23d0b73c08e17686b3698036eb
tuguldurs <tuguldur.s@gmail.com>
2022-02-08 22:21:36 -0500

    logging

--- viewer.py
+++ viewer.py
@@ -1,10 +1,16 @@
 from __future__ import annotations
 
-
 import argparse
+import logging
 from dataclasses import dataclass
 
 from pydicom import dcmread
+
+
+log  = logging.getLogger(__name__)
+fmt = '%(asctime)s ~ %(name)14s ~ %(levelname)8s ::: %(message)s'
+lvl = logging.INFO
+logging.basicConfig(level=lvl, format=fmt)
 
 
 @dataclass
@@ -22,6 +28,9 @@ class PullBack:
     def __post_init__(self) -> None:
         self.dcm = dcmread(self.series_name)
         self.video = self.dcm.pixel_array
+        self.n_frames = len(self.video)
+        log.info(f'DICOM data read from {self.series_name}')
+        log.info(f'number of frames = {self.n_frames}')
 
 
 def main(args) -> None:
