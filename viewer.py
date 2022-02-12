3ed8ec1425258df9efe8c67e9b51a16d4ab87af6
tuguldurs <tuguldur.s@gmail.com>
2022-02-12 17:17:03 -0500

    scale remover

--- viewer.py
+++ viewer.py
@@ -48,12 +48,14 @@ class WidgetCoords:
         search_in - Header tag search input textbox widget coordinates.
         search_out - Header tag search output textbox widget coordinates.
         header - Full header display button widget coordinates.
+        rmscale - Scale remover button widget coordinates.
     """
 
     slider     = [0.23, 0.02, 0.56, 0.04]
     search_in  = [0.20, 0.90, 0.25, 0.08]
     search_out = [0.50, 0.90, 0.40, 0.08]
     header     = [0.80, 0.80, 0.20, 0.04]
+    rmscale    = [0.80, 0.70, 0.20, 0.04]
 
 
 class WidgetCreator:
@@ -94,13 +96,23 @@ class WidgetCreator:
 
     @staticmethod
     def header(coords) -> Button:
-        """Creates header button."""
+        """Creates header viewer button."""
         ax_header = plt.axes(coords)
         header = Button(
             ax_header, 'Show Full Header', 
             hovercolor='0.975'
             )
         return header
+
+    @staticmethod
+    def rmscale(coords) -> Button:
+        """Creates scale remover button."""
+        ax_rmscale = plt.axes(coords)
+        rmscale = Button(
+            ax_rmscale, 'Remove Scale', 
+            hovercolor='0.975'
+            )
+        return rmscale
 
 
 class Viewer:
@@ -123,11 +135,13 @@ class Viewer:
         self.search_in = self.widget.search_in(self.coords.search_in)
         self.search_out = self.widget.search_out(self.coords.search_out)
         self.header = self.widget.header(self.coords.header)
+        self.rmscale = self.widget.rmscale(self.coords.rmscale)
 
         # actions
         self.slider.on_changed(self.update_frame)
         self.search_in.on_submit(self.submit)
         self.header.on_clicked(self.open_header)
+        self.rmscale.on_clicked(self.remove_scale)
 
         plt.show()
 
@@ -142,6 +156,7 @@ class Viewer:
         """Updates image with current frame from slider."""
         current_idx = int(round(self.slider.val))
         self.img.set_data(self.pb.video[current_idx])
+        self.current_idx = current_idx
 
     def submit(self, tag_name) -> None:
         """Searches for a tag by name in header and 
@@ -168,6 +183,15 @@ class Viewer:
         else:
             opener = "open" if sys.platform == "darwin" else "xdg-open"
         subprocess.call([opener, filename])
+
+    def remove_scale(self, event) -> None:
+        """Removes scale marks from current frame."""
+        raw_frame = self.pb.video[self.current_idx]
+        red_channel = np.dot(raw_frame[... , :3] , [1, 0, 0])
+        avg_signal = np.dot(raw_frame[... , :3] , [1, 1, 1]) / 3
+        diff = red_channel - avg_signal
+        raw_frame[diff > 0] = np.array([0,0,0])
+        self.img.set_data(raw_frame)
 
 
 if __name__ == '__main__':
